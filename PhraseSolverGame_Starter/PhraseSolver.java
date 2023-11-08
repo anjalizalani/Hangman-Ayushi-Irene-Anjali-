02bbc3dbfb3d77ca33b354b639c89f9ef4bf806b
anjalizalani <142432215+anjalizalani@users.noreply.github.com>
2023-11-07 21:31:38 -0800

    Update PhraseSolver.java

--- PhraseSolverGame_Starter/PhraseSolver.java
+++ PhraseSolverGame_Starter/PhraseSolver.java
@@ -8,8 +8,17 @@ import java.util.Scanner;
 public class PhraseSolver
 {
   /* your code here - attributes */
+  private Player player1; 
+  private Player player2; 
+  private Board game; 
+  private int playerGuessPhrase;
 
   /* your code here - constructor(s) */ 
+  public PhraseSolver(){ 
+    player1 = new Player(); 
+    player2 = new Player(); 
+    game = new Board(); 
+  }
 
   /* your code here - accessor(s) */
   
@@ -27,6 +36,15 @@ public class PhraseSolver
     {
       
       /* your code here - game logic */
+      String guess = ""; 
+      boolean guessPhrase = false; 
+      while (currentPlayer == 1){ 
+        System.out.println("Current phrase solved: " + game.getSolvedPhrase()); 
+        System.out.println("Point value of the next letter guess: " + game.getCurrentLetterValue()); 
+        System.out.println("Current player name: " + player1.getName()); 
+        System.out.println("Player points: " + player1.getPoints()); 
+        System.out.println("Type in a guess(type in a letter, for guessing the entire phrase or when it is revealed, type in \" try phrase\"): "); 
+        guess = input.nextLine();
       
       
       /* your code here - determine how game ends */
