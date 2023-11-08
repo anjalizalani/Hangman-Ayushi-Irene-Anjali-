a195827b0e366d994236aea0500abf5d1f87a712
anjalizalani <142432215+anjalizalani@users.noreply.github.com>
2023-11-07 21:32:50 -0800

    Update Player.java

--- PhraseSolverGame_Starter/Player.java
+++ PhraseSolverGame_Starter/Player.java
@@ -8,8 +8,15 @@ import java.util.Scanner;
 public class Player
 {
   /* your code here - attributes */
-
+  private String name = "";
+  private double points;
   /* your code here - constructor(s) */ 
+  public Player(){ 
+    System.out.println("Please input your name:"); 
+    Scanner sc = new Scanner(System.in); 
+    name = sc.nextLine(); 
+    points = 0; 
+    System.out.println("Welcome to the game " + name + "!");
 
   /* your code here - accessor(s) */ 
 
